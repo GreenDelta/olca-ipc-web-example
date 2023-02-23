561c42afef0a4eaf84d87f231f8059e2bf1ba38c
msrocka <michael.srocka@gmail.com>
2023-02-23 16:33:35 +0100

    build a connection panel

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-
+https://picocss.com
 https://github.com/visgl/deck.gl
 https://github.com/visgl/deck.gl/tree/8.8-release/examples/website/line
 https://deck.gl/examples/line-layer/
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as o from "olca-ipc";
 
+interface State {
+  client: o.IpcClient | o.RestClient;
+}
+
 async function main() {
 
-  ReactDOM.render(<h1>Works</h1>, document.getElementById("app"));
+  ReactDOM.render(<ConnectionPanel />, document.getElementById("app"));
 
   /*
   const client = o.IpcClient.on(8080);
@@ -14,5 +18,54 @@ async function main() {
   })
   */
 };
+
+const ConnectionPanel = () => {
+
+  type Protocol = "JSON-RPC" | "REST";
+
+  const [url, setUrl] = React.useState("http://localhost:8080");
+  const [protocol, setProtocol] = React.useState<Protocol>("JSON-RPC");
+  const protocols: Protocol[] = ["JSON-RPC", "REST"];
+
+  const onConnect = () => {
+    console.log(`connect: ${protocol} :: ${url}`);
+    const client = protocol === "JSON-RPC"
+      ? o.IpcClient.on(url)
+      : o.RestClient.on(url);
+  };
+
+  return <article>
+    <h3>Connect to an openLCA server</h3>
+    <form>
+      <div className="grid">
+        <label htmlFor="url">
+          URL
+          <input type="url" id="url" value={url}
+            onChange={(e) => setUrl(e.target.value)}>
+          </input>
+        </label>
+        <label htmlFor="protocol">
+          Protocol
+          <select value={protocol}
+            onChange={(e) => setProtocol(e.target.value as Protocol)}>
+            {protocols.map(p => <option>{p}</option>)}
+          </select>
+        </label>
+      </div>
+      <div className="grid">
+        <div />
+        <div className="grid">
+          <div />
+          <div />
+          <a href="#" role="button" className="contrast outline"
+            onClick={() => onConnect()}>
+            Connect
+          </a>
+        </div>
+      </div>
+    </form>
+  </article>
+};
+
 
 main();
