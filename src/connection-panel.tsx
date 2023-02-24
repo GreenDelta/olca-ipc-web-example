c4994a96cd0b90654115108f3c01ef49318e7eea
msrocka <michael.srocka@gmail.com>
2023-02-24 10:11:53 +0100

    more state

---
+++ src/connection-panel.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import * as o from "olca-ipc";
+
+type Client = o.IpcClient | o.RestClient;
+
+export const ConnectionPanel = (
+    props: { onConnected: (client: Client) => void }) => {
+
+    type Protocol = "JSON-RPC" | "REST";
+
+    const [url, setUrl] = React.useState("http://localhost:8080");
+    const [protocol, setProtocol] = React.useState<Protocol>("JSON-RPC");
+    const protocols: Protocol[] = ["JSON-RPC", "REST"];
+
+    const onConnect = () => {
+        console.log(`connect: ${protocol} :: ${url}`);
+        const client = protocol === "JSON-RPC"
+            ? o.IpcClient.on(url)
+            : o.RestClient.on(url);
+        props.onConnected(client);
+    };
+
+    return <article>
+        <h3>Connect to an openLCA server</h3>
+        <p>
+            This will try to connect to an openLCA IPC server at the given
+            URL using the selected protocol. For example, you can start
+            an IPC server from the openLCA user interface under <strong>
+                Tools &gt; Developer tools &gt; IPC Server</strong> and
+            connect to it with the default settings. In order to run a
+            calculation example, the connected database of that server
+            needs to contain at least one product system and impact
+            assessment method.
+        </p>
+        <form>
+            <div className="grid">
+                <label htmlFor="url">
+                    URL
+                    <input type="url" id="url" value={url}
+                        onChange={(e) => setUrl(e.target.value)}>
+                    </input>
+                </label>
+                <label htmlFor="protocol">
+                    Protocol
+                    <select value={protocol}
+                        onChange={(e) => setProtocol(e.target.value as Protocol)}>
+                        {protocols.map(p => <option>{p}</option>)}
+                    </select>
+                </label>
+            </div>
+            <div className="grid">
+                <div />
+                <div className="grid">
+                    <div />
+                    <div />
+                    <a href="#" role="button" className="contrast outline"
+                        onClick={() => onConnect()}>
+                        Connect
+                    </a>
+                </div>
+            </div>
+        </form>
+    </article>
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,95 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as o from "olca-ipc";
+import { ConnectionPanel } from "./connection-panel";
+
+type Client = o.IpcClient | o.RestClient;
 
 interface State {
-  client: o.IpcClient | o.RestClient;
+  client?: Client;
+  error?: string;
 }
+
+
+
+const App = () => {
+  const [state, setState] = React.useState<State>({});
+  const [progress, setProgress] = React.useState<string | null>(null);
+
+  const reset = () => {
+    setState({});
+    setProgress(null);
+  }
+
+  React.useEffect(() => {
+    if (!state.client) {
+      return;
+    }
+    const client = state.client!;
+    setProgress("Connecting ...");
+    (async () => {
+      try {
+        const systems = await client.getDescriptors(o.RefType.ProductSystem);
+        const methods = await client.getDescriptors(o.RefType.ImpactMethod);
+        if (!systems || systems.length === 0) {
+          setState({ error: "Could not find any product systems" });
+          return;
+        }
+        if (!methods || methods.length === 0) {
+          setState({ error: "Could not find any product systems" });
+          return;
+        }
+        setProgress(null);
+      } catch (e) {
+        setState({ error: `Connection failed: ${e}` });
+      }
+    })();
+
+    console.log("client set!");
+  }, [state.client]);
+
+  if (state.error) {
+    return <ErrorPanel message={state.error} onReset={reset} />;
+  }
+  if (progress) {
+    return <ProgressPanel message={progress} />;
+  }
+  if (!state.client) {
+    return <ConnectionPanel
+      onConnected={(client) => setState({ ...state, client })} />;
+  }
+  return <ErrorPanel message="Unhandled state" onReset={reset} />;
+};
+
+const ErrorPanel = ({ message, onReset }:
+  { message: string, onReset: () => void }) => {
+  return <article>
+    <h3 style={{ color: "#f06292" }}>Error</h3>
+    <kbd>{message}</kbd>
+    <div className="grid">
+      <div />
+      <div className="grid">
+        <div />
+        <a href="#" role="button" className="contrast outline"
+          onClick={() => onReset()}>
+          Try again
+        </a>
+        <div />
+      </div>
+    </div>
+  </article>;
+};
+
+const ProgressPanel = ({ message }: { message: string }) => {
+  return <article>
+    <h3>{message}</h3>
+    <progress />
+  </article>;
+};
 
 async function main() {
 
-  ReactDOM.render(<ConnectionPanel />, document.getElementById("app"));
+  ReactDOM.render(<App />, document.getElementById("app"));
 
   /*
   const client = o.IpcClient.on(8080);
@@ -18,54 +99,5 @@ async function main() {
   })
   */
 };
-
-const ConnectionPanel = () => {
-
-  type Protocol = "JSON-RPC" | "REST";
-
-  const [url, setUrl] = React.useState("http://localhost:8080");
-  const [protocol, setProtocol] = React.useState<Protocol>("JSON-RPC");
-  const protocols: Protocol[] = ["JSON-RPC", "REST"];
-
-  const onConnect = () => {
-    console.log(`connect: ${protocol} :: ${url}`);
-    const client = protocol === "JSON-RPC"
-      ? o.IpcClient.on(url)
-      : o.RestClient.on(url);
-  };
-
-  return <article>
-    <h3>Connect to an openLCA server</h3>
-    <form>
-      <div className="grid">
-        <label htmlFor="url">
-          URL
-          <input type="url" id="url" value={url}
-            onChange={(e) => setUrl(e.target.value)}>
-          </input>
-        </label>
-        <label htmlFor="protocol">
-          Protocol
-          <select value={protocol}
-            onChange={(e) => setProtocol(e.target.value as Protocol)}>
-            {protocols.map(p => <option>{p}</option>)}
-          </select>
-        </label>
-      </div>
-      <div className="grid">
-        <div />
-        <div className="grid">
-          <div />
-          <div />
-          <a href="#" role="button" className="contrast outline"
-            onClick={() => onConnect()}>
-            Connect
-          </a>
-        </div>
-      </div>
-    </form>
-  </article>
-};
-
 
 main();
